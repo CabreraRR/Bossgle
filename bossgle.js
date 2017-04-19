e3e76e7abe7fc96bc814c29dad13a53c0a16eff6
Roel <roel.cabrera@gmail.com>
2017-04-19 13:46:15 -0700

    😑

--- bossgle.js
+++ bossgle.js
@@ -1,21 +1,63 @@
 var score = 0;
 var letterContainer = [];
 var letters = [];
-function randomLetters(){
+var active_letters = [];
+var word_list = [];
 
-  for (var i=0; i < 16; i++)
+function randomLetters() {
     var num = Math.floor(Math.random()*26);
     var letter = String.fromCharCode(num + 65);
-  letters.push(letter);
   return letter;
+}
+function display_letters(){
+  $('#letter-container').html(active_letters.join(''));
 }
 
 $(function(){
   $('.flex-item.box').on('click', function(e){
-    console.log(this.innerHTML);
-  }).each((i,item)=>{
-    var $this = $(item);
-    $this.html(randomLetters());
+    if($(this).hasClass('box-disabled')) return;
+    //If the jquery wrapped object that this element
+    active_letters.push(this.innerHTML);
+    console.log(active_letters);
+    display_letters();
+  }).each(function(i,item){
+    var $this = $(item),
+        letter = randomLetters();
+    //this function handles the initial setup.
+    while(letters.indexOf(letter)!=-1){
+      letter = randomLetters();
+    }
+    letters.push(letter);
+    $this.html(letter);
   });
 });
 
+for(var i=0;i<$('.flex-item').length;i++){
+  item = $('.flex-item')[i];
+}
+
+$(function(){
+  $('.btn-danger').on('click', function (e){
+    if($(this).hasClass('box-disabled')) return;
+    active_letters.length = 0;
+    display_letters();
+    console.log (active_letters);
+  })
+});
+
+//$(function(){
+//  $('.btn-warning').on('click', functiontion (e)){
+//
+//});
+
+$(function(){
+  $('btn-primary').on('click', function (e){
+    var word = active_letters.join('')
+//    if($(this).hasClass('box-disabled')) return;
+    if (isBasicWord(word)) {
+      word_list.push(word);
+      console.log(word_list)
+    }
+    
+  });
+});
